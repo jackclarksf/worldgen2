00cbb15099e023f42f8c19d09174ea10f2f6b3ae
mappingbabel <iamjackc@gmail.com>
2016-02-11 22:27:03 -0800

    freakymapfiddling

--- worlds.py
+++ worlds.py
@@ -1,6 +1,6 @@
 __author__ = 'iamja_000'
 
-from itertools import product
+from itertools import product, starmap
 import random
 
 class World:
@@ -11,9 +11,9 @@ class World:
         self.terrain_b = []
         self.terrain_list = [self.terrain_a, self.terrain_b]
         self.terrain_scatter()
-        print(self.terrain_list)
-        print(self.return_dominant())
-        print(self.return_submissive())
+        print("Test clump")
+        self.terrain_clump()
+        print("End test clump")
 
 
 
@@ -23,11 +23,23 @@ class World:
         w_coord = list(product(range(self.x), range(self.y)))
         return w_coord
 
+    def get_neighbours(self, x_coord, y_coord):
+        cells = starmap(lambda a,b: (x_coord+a, y_coord+b), product((0,-1,+1), (0,-1,+1)))
+        return list(cells)[1:]
+
     def terrain_scatter(self):
         base_coords = self.world_coordinates()
         for i in base_coords:
             choice = random.randint(0, 1)
             self.terrain_list[choice].append(i)
+
+    def terrain_clump(self):
+        min_list = list(self.return_submissive())
+        for i, j in enumerate(min_list):
+            print(i)
+            print(j)
+
+
 
     def return_dominant(self):
         return max(self.terrain_list, key=len)
