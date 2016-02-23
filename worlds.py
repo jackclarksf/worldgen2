10839a228b45031cc3cd3031dad94bebfc564ead
mappingbabel <iamjackc@gmail.com>
2016-02-22 20:33:50 -0800

    island generation

--- worlds.py
+++ worlds.py
@@ -12,10 +12,10 @@ class World:
     def __init__(self, x, y):
         self.x = int(x)
         self.y = int(y)
+        total_squares = self.x * self.x
         water_level = random.randint(0, 10)
-        print("World born with size: {} comprising of {} squares and a water level of {}".format(self.x, self.x*self.x, water_level))
+        print("World born with size: {} comprising of {} squares and a water level of {}".format(self.x, total_squares, water_level))
         self.water_list = self.water_list_generator(water_level)
-        print(self.water_list)
 
 
     def water_return(self):
@@ -52,7 +52,23 @@ class World:
                 water_list.append(i)
             elif b == self.x-1:
                 water_list.append(i)
+        remaining_list = [x for x in our_coordinates if x not in water_list]
+        #print(remaining_list)
+        extra_list = self.island_function(remaining_list)
+        water_list = extra_list + water_list
         return water_list
+
+    def island_function(self, island_list):
+        additional_island_list = []
+        for i in island_list:
+            water_roll = random.randint(0, 10)
+            a, b = i
+            if a in range(1, self.x-1):
+                if water_roll > 5:
+                    additional_island_list.append(i)
+        #print("Our extra list is {}".format(additional_island_list))
+        return additional_island_list
+
 
     def land_world(self, w_level):
         water_list = []
